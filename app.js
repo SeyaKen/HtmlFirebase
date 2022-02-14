20e4fcd76e53e8d47be25534c8b644ea716df5c1
SeyaKen <75517657+SeyaKen@users.noreply.github.com>
2022-02-14 16:43:09 +0900

    firebase auth completed

--- app.js
+++ app.js
@@ -25,6 +25,42 @@ function loadbody() {
   spanWeekday.innerText=weekday;
 }
 
+// signUp
+const signupForm = document.getElementById('signup-form');
+// 対象要素.addEventListener( イベントの種類, 関数, false )
+// 上で取得した、signup-form要素のsubmitが押された時
+// 2番目の引数に指定した関数が発火する
+signupForm.addEventListener('submit', e=>{
+  e.preventDefault();
+  // signupForm['獲得したい値のidを指定する']
+  const name = signupForm['name'].value;
+  const email = signupForm['email'].value;
+  const password = signupForm['password'].value;
+  console.log(name, email, password);
+  signupForm.reset();
+  auth.createUserWithEmailAndPassword(email, password).then(cred=>{
+    return db.collection('people').doc(cred.user.uid).set({
+      Name: name,
+      Email: email,
+      Password: password,
+    }).then(() => {
+      console.log('ログインに成功しました。');
+      location = 'index.html';
+    }).catch(err => {
+      console.log(err.message);
+      const signupError = document.getElementById('signupError');
+      signupError.innerText = err.message;
+      console.log('ログインに失敗しました。');
+    }).catch(err => {
+      console.log(err.message);
+      const signupError2 = document.getElementById('signupError2');
+      signupError2.innerText = err.message;
+      console.log('ログインに失敗しました。');
+    });
+  });
+});
+
+
 
 
 
